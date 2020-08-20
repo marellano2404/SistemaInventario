f79da0cb85fa904d26f52cd06291231efbf2f7b6
fragoso94 <ing.fragoso94@gmail.com>
2020-08-19 22:30:34 -0500

    Se implemento el obtener un articulo por Id

--- Almacen.Core/BL/Articulos/Services/ArticuloService.cs
+++ Almacen.Core/BL/Articulos/Services/ArticuloService.cs
@@ -11,9 +11,51 @@ namespace Almacen.Core.BL.Articulos.Services
 {
     public class ArticuloService : IArticulo
     {
-        public Task<Articulo> ObtenerArticulo(string Id)
+        public async Task<Articulo> ObtenerArticulo(string Id)
         {
-            throw new NotImplementedException();
+            using (var conexion = new SqlConnection(Helpers.ContextConfiguration.ConexionString))
+            {
+                Articulo respuesta = new Articulo();
+                try
+                {
+                    var command = new SqlCommand();
+                    command.Connection = conexion;
+                    command.CommandText = "Catalogos.ObtenerArticulos";
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@OP", "ObtenerArticulo");
+                    command.Parameters.AddWithValue("@IdArticulo", Id);
+                    conexion.Open();
+                    var lectura = await command.ExecuteReaderAsync();
+                    if (lectura.HasRows) //que tenga renglones
+                    {
+                        while (lectura.Read())
+                        {
+                            respuesta.Id = lectura.GetGuid(0);
+                            respuesta.Descripcion = lectura.GetString(1);
+                            respuesta.ClaveProducto = lectura.GetString(2);
+                            respuesta.Detalles = lectura.GetString(3);
+                            respuesta.Presentacion = lectura.GetString(4);
+                            respuesta.Marca = lectura.GetString(5);
+                            respuesta.Modelo = lectura.GetString(6);
+                            respuesta.TipoMedicamento = lectura.GetInt32(7);
+                            respuesta.TipoCatalogo = lectura.GetString(8);
+                            respuesta.UnidadMedida = lectura.GetInt32(9);
+                            respuesta.CantidadPorUnidad = lectura.GetInt32(10);
+                            respuesta.CodigoBarras = lectura.GetString(11);
+                            respuesta.Laboratorio = lectura.GetInt32(12);
+                            respuesta.Estado = lectura.GetInt32(13);
+                        }
+                    }
+                    conexion.Close();
+                    return respuesta;
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message.ToString());
+                    return respuesta;
+                }
+            }
         }
 
         public async Task<List<Articulo>> ObtenerArticulos(Pagination P)
