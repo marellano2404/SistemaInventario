913a152e9bd79d8aecc54c07a4ebbad1ac5a9a4e
fragoso94 <ing.fragoso94@gmail.com>
2020-08-27 22:13:13 -0500

    se agrego la configuración para utilizar Rotativa y generar PDFs

---
+++ Almacen.Portal/Controllers/CrearPDFController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Rotativa.AspNetCore;
+
+namespace Almacen.Portal.Controllers
+{
+    public class CrearPDFController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return View();
+            //var datos = await ObtenerUsuarios();
+            return new ViewAsPdf("Index")
+            {
+                PageSize = Rotativa.AspNetCore.Options.Size.Letter,
+                PageOrientation = Rotativa.AspNetCore.Options.Orientation.Portrait
+                //FileName = "ReporteUsuarios.pdf"
+            };
+        }
+    }
+}
